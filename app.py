9d41e968989846033eadb1e108aaa6efac2ae8ce
Andrew Sanetra <asanetras@gmail.com>
2017-03-08 19:00:44 -0600

    Edit urls and methods for query input

--- app.py
+++ app.py
@@ -9,13 +9,17 @@ app = Flask(__name__)
 def home():
     return "Looks like you're lost, friend!"
 
-@app.route("/response/<text>", methods=["GET"])
-def respond_get(text):
+@app.route("/q/<text>", methods=["GET"])
+def text_query_get(text):
     return intella.parse_input(text)
 
-@app.route("/response", methods=["POST"])
-def respond_post():
-    return intella.parse_input(request.form("text"))
+@app.route("/q", methods=["POST"])
+def text_query_post():
+    return intella.parse_input(request.form("q"))
+
+@app.route("/vq", methods=["POST"])
+def voice_query_post():
+    return intella.parse_voice(request.form)
 
 if __name__ == '__main__':
     app.run()
--- intella.py
+++ intella.py
@@ -2,3 +2,6 @@ import nltk
 
 def parse_input(raw_text):
     return str(nltk.pos_tag(nltk.word_tokenize(raw_text)))
+
+def parse_voice(form):
+    pass

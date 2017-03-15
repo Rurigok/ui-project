454ebe66536748fdc1334566bdccf8de3036500f
Andrew Sanetra <asanetras@gmail.com>
2017-03-15 14:55:45 -0500

    Add basic noun/verb parsing functionality
    The server now returns a response with a list of nouns and verbs given in
    the input request. A convienence GET method has been added for easy
    testing of the form: /q/<input query>.

--- server/app.py
+++ server/app.py
@@ -9,9 +9,16 @@ app = Flask(__name__)
 def home():
     return "Looks like you're lost, friend!"
 
+@app.route("/q/<q>")
+def text_query(q):
+    return query.parse_query(q, None, None)
+
 @app.route("/q", methods=["POST"])
 def text_query_post():
-    return query.parse_input(request.form("q"))
+    query_text = request.form("q")
+    location = request.form("l")
+    timestamp = request.form("t")
+    return query.parse_query(query_text, location, timestamp)
 
 @app.route("/vq", methods=["POST"])
 def voice_query_post():
--- server/query.py
+++ server/query.py
@@ -1,7 +1,20 @@
 import nltk
 
 def parse_query(raw_text, location, timestamp):
-    return str(nltk.pos_tag(nltk.word_tokenize(raw_text)))
+    tags = nltk.pos_tag(nltk.word_tokenize(raw_text))
+    # Parse nouns
+    nouns = []
+    verbs = []
+
+    for pair in tags:
+        word = pair[0]
+        part_of_speech = pair[1]
+        if part_of_speech.startswith("N"):
+            nouns.append(word)
+        if part_of_speech.startswith("V"):
+            verbs.append(word)
+
+    return "nouns: {}\nverbs: {}".format(nouns, verbs)
 
 def parse_voice(form):
     pass

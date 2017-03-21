0921f95f643ca9bfb52b0cfe4eb983b29c7ce168
Edward Mondragon <insomnawolf@gmail.com>
2017-03-21 18:03:45 -0500

    handleButton now works       added View parameter
     prints the query to console

--- client/VAClient/mobile/src/main/java/finalproject/ui/vaclient/MainActivity.java
+++ client/VAClient/mobile/src/main/java/finalproject/ui/vaclient/MainActivity.java
@@ -1,26 +1,32 @@
 package finalproject.ui.vaclient;
 
+import android.media.Image;
 import android.support.annotation.XmlRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    String txtLine;
+    EditText txtLine;
+    ImageButton process;
+    String str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtLine = findViewById(R.id.queryLine).toString();
-        System.out.println(txtLine);
+        txtLine = (EditText) findViewById(R.id.queryLine);
+        process = (ImageButton) findViewById(R.id.processButton);
     }
 
-    public void handleButton()
+    public void handleButton(View v)
     {
-        System.out.println("HI :D" + txtLine);
+        str = txtLine.getText().toString();
+        System.out.println(str);
     }
 }

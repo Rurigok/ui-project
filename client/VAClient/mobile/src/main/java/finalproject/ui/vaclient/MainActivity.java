2aa70d8b764d6f8bb31cd4a422c771888795a147
Edward Mondragon <insomnawolf@gmail.com>
2017-03-21 17:51:13 -0500

    Gave ID to the button: "processButton".
    Added a handleButton method.
    //handleButton doesn't work yet...

--- client/VAClient/mobile/src/main/java/finalproject/ui/vaclient/MainActivity.java
+++ client/VAClient/mobile/src/main/java/finalproject/ui/vaclient/MainActivity.java
@@ -1,13 +1,26 @@
 package finalproject.ui.vaclient;
 
+import android.support.annotation.XmlRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    String txtLine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtLine = findViewById(R.id.queryLine).toString();
+        System.out.println(txtLine);
+    }
+
+    public void handleButton()
+    {
+        System.out.println("HI :D" + txtLine);
     }
 }
